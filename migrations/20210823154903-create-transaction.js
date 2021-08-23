517790c4b67781760ac32963763a265d886cbe06
CyrilBaah <cyril.baah@amalitech.org>
2021-08-23 15:49:54 +0000

    Transaction - model created

---
+++ migrations/20210823154903-create-transaction.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      date: {
+        type: Sequelize.STRING
+      },
+      paymentAmount: {
+        type: Sequelize.STRING
+      },
+      paymentRecipient: {
+        type: Sequelize.STRING
+      },
+      envelopeId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Transactions');
+  }
+};
