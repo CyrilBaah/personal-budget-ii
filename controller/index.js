6d9eb617aa687b1e08810f1df0790505ab20fd75
CyrilBaah <cyril.baah@amalitech.org>
2021-08-24 21:40:12 +0000

    Code refactor - includes folder created

--- controller/index.js
+++ controller/index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 
 exports.getHomePage = async (req, res) => {
-    res.render('index', {
+    res.render('envelope/index', {
         title: 'Envelope Budgeting API',
-        path: '/index'
+        path: '/index',
+        pageName: 'Create an envelope'
+    });
+}
+
+exports.getEnvelopes = async (req, res) => {
+    res.render('envelope/get', {
+        title: 'Envelope Budgeting API',
+        path: '/get',
+        pageName: 'Get all envelopes'
     });
 }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const indexController = require('../controller/index');
 
 router.get('/', indexController.getHomePage);
+router.get('/get-envelopes', indexController.getEnvelopes);
+
 
 
 module.exports = router;
