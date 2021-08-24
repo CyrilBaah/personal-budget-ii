5741ed680fa20a30ac4450d95f6d92798e980aad
CyrilBaah <cyril.baah@amalitech.org>
2021-08-24 12:02:59 +0000

    Swagger set up

--- app.js
+++ app.js
@@ -4,6 +4,34 @@ const { sequelize } = require('./models');
 require('dotenv').config();
 app.use(express.json());
 
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const swaggerDefinition = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Personal Budget II",
+            version: "1.0.0",
+            description: "An API that allows clients to create and manage a personal budget using Envelope Enveloping Budgeting"
+        },
+        servers: [
+            {
+                url: "http://localhost:3000"
+            },
+        ],
+    }
+}
+
+const options = {
+    swaggerDefinition,
+    apis: ["./controller/envelope.js", "./controller/transaction.js"]
+};
+
+const swaggerSpecification = swaggerJsDoc(options);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpecification));
+
+
 const envelopeRoutes = require('./routes/envelope');
 const transactionRoutes = require('./routes/transaction');
 
